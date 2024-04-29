9cefb9fe17fb6164010061a9c99c8aac23954152
StephenKiiyuruNganga <steveknganga@gmail.com>
2024-04-29 14:53:03 +0300

    add themme toggle

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,50 @@
+import * as React from "react"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export function ThemeToggle({ children }: { children: React.ReactNode }) {
+  const [theme, setThemeState] = React.useState<
+    "theme-light" | "dark" | "system"
+  >("theme-light")
+
+  React.useEffect(() => {
+    const isDarkMode = document.documentElement.classList.contains("dark")
+    setThemeState(isDarkMode ? "dark" : "theme-light")
+  }, [])
+
+  React.useEffect(() => {
+    const isDark =
+      theme === "dark" ||
+      (theme === "system" &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    document.documentElement.classList[isDark ? "add" : "remove"]("dark")
+  }, [theme])
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          {children}
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setThemeState("theme-light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setThemeState("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setThemeState("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
---
+++ src/data/siteData.ts
@@ -0,0 +1,24 @@
+export const baseData = {
+  title: "Imla",
+  description: `Save time, Do more. 
+  Automatic speech recognition software tailored for Africa.
+  `,
+}
+
+export const socialData = [
+  {
+    href: "https://github.com/skiiyuru",
+    icon: "github",
+    title: "Github",
+  },
+  {
+    href: "https://www.linkedin.com/in/skiiyuru/",
+    icon: "linkedin",
+    title: "Linked In",
+  },
+  {
+    href: "https://twitter.com/snkiiyuru",
+    icon: "twitter",
+    title: "Twitter",
+  },
+]
