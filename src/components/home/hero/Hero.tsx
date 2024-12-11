44574cd6d0bc7ca168614901853da0b3af290331
StephenKiiyuruNganga <steveknganga@gmail.com>
2024-12-11 13:18:16 +0300

    add responsiveness to hero section

--- src/components/home/hero/Hero.tsx
+++ src/components/home/hero/Hero.tsx
@@ -2,23 +2,29 @@ import { ContainerScroll } from "@/components/home/hero/container-scroll-animati
 
 import type { ReactNode } from "react"
 
+function HeroTitle() {
+  return (
+    <h1 className="text-4xl text-foreground text-balance">
+      Unleash the power of
+      <br />
+      <span className="text-5xl md:text-[6rem] font-black mt-1 leading-none font-serif text-balance text-primary">
+        Imla Transcripts
+      </span>
+    </h1>
+  )
+}
 export default function Hero({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col overflow-hidden">
-      <ContainerScroll
-        titleComponent={
-          <h1 className="text-4xl  text-foreground text-balance">
-            Unleash the power of
-            <br />
-            <span className="text-5xl md:text-[6rem] font-black mt-1 leading-none font-serif text-balance">
-              Imla Transcripts
-            </span>
-          </h1>
-        }
-      >
-        {children}
-      </ContainerScroll>
-    </div>
+    <>
+      <div className="container text-center md:hidden space-y-14 lg:space-y-32 my-32">
+        <HeroTitle />
+      </div>
+      <div className="hidden md:flex flex-col overflow-hidden">
+        <ContainerScroll titleComponent={<HeroTitle />}>
+          {children}
+        </ContainerScroll>
+      </div>
+    </>
   )
 }
 
