ab26928427f41c14a28ad2e68375cd80a8d63598
StephenKiiyuruNganga <steveknganga@gmail.com>
2025-03-25 17:11:53 +0300

    refactor(hero): update mobile and ui mocks

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,7 +10,7 @@ const {
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
+  darkMode: "class",
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
